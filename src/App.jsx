7f532446128762842136d983cff274b220984218
viltlaravel <nicoleamoguis15@gmail.com>
2025-01-16 21:21:45 +0800

    feat: implemented seo

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,45 @@ import {
   StarsCanvas,
 } from "./components";
 import { ToastContainer } from "react-toastify";
+import { Helmet } from "react-helmet-async";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>Nicole N. Amoguis | Software Engineer Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to the portfolio of Nicole N. Amoguis, a software engineer specializing in web and mobile development. Explore projects and skills."
+        />
+        <meta
+          name="keywords"
+          content="Nicole Amoguis, portfolio, software engineer, web development, mobile development, web developer, front-end developer"
+        />
+        <meta
+          property="og:title"
+          content="Nicole N. Amoguis | Software Engineer Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Explore Nicole's software engineering projects and skills."
+        />
+        <meta property="og:image" content="/seo/img-hero.png" />
+        <meta
+          property="og:url"
+          content="https://nicole-amoguis-portfolio.vercel.app"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Nicole N. Amoguis | Software Engineer"
+        />
+        <meta
+          name="twitter:description"
+          content="Discover Nicole's skills in mobile and web development."
+        />
+        <meta name="twitter:image" content="/seo/img-hero.png" />
+      </Helmet>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
