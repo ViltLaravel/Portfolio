376d47253923a5ca8e273c674dbf0844dfa189be
viltlaravel <nicoleamoguis15@gmail.com>
2026-01-02 19:15:53 +0800

    edit headline text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -70,7 +70,7 @@ export default function Hero() {
           <Typewriter
             options={{
               strings: [
-                "Frontend Developer",
+                "Full Stack Developer",
                 "Upwork-Freelancer",
                 "UI/UX Designer",
                 "2+ years of experience",
